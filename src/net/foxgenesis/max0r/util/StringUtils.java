beb10bc16a1fc06f58c3f39083c88b096a82ca39
Ashley <ashley@foxgenesis.net>
2022-11-23 20:54:07 -0600

    Update StringUtils.java

--- src/net/foxgenesis/max0r/util/StringUtils.java
+++ src/net/foxgenesis/max0r/util/StringUtils.java
@@ -20,14 +20,9 @@ public final class StringUtils {
 	 * {@code \b((http|https|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])}
 	 * </blockquote>
 	 */
-	public static final Pattern URL_REGEX = Pattern.compile(
+	public static final Pattern PATTERN_URL = Pattern.compile(
 			"\\b((http|https|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])",
 			Pattern.CASE_INSENSITIVE);
-
-	/**
-	 * NEED_JAVADOC
-	 */
-	public static final Predicate<String> CONTAINS_URL = URL_REGEX.asPredicate();
 
 	/**
 	 * Compiled regex checking for URL character sequences. This pattern is case
@@ -36,9 +31,14 @@ public final class StringUtils {
 	 * {@code \b(?<protocol>http|https|ftp|file)://(?<domain>[-a-zA-Z0-9+&@#%?=~_|!:,.;]*[-a-zA-Z0-9+&@#%=~_|])(?<path>\S*)}
 	 * </blockquote>
 	 */
-	public static final Pattern URL_REGEX_NAMED_GROUP = Pattern.compile(
+	public static final Pattern PATTERN_URL_WITH_GROUPING = Pattern.compile(
 			"\\b(?<protocol>http[s]?|ftp|file)://(?<domain>[-a-zA-Z0-9+&@#%?=~_|!:,.;]*[-a-zA-Z0-9+&@#%=~_|])(?<path>\\S*)",
 			Pattern.CASE_INSENSITIVE);
+	
+	/**
+	 * NEED_JAVADOC
+	 */
+	public static final Predicate<String> CONTAINS_URL = PATTERN_URL.asPredicate();
 
 	/**
 	 * Find all URLs within a String and split its components into groups.
@@ -49,6 +49,6 @@ public final class StringUtils {
 	 *         {@code path}({@code 3})
 	 */
 	public static Stream<MatchResult> findURLWithGroups(@Nonnull String str) {
-		return URL_REGEX_NAMED_GROUP.matcher(str).results();
+		return PATTERN_URL_WITH_GROUPING.matcher(str).results();
 	}
 }
