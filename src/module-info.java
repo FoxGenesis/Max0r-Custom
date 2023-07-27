aca7e55fcb2bafa07db943c76e20993cb195c5d6
Ashley <ashley@foxgenesis.net>
2023-07-27 17:40:45 -0500

    import cleanup

--- src/module-info.java
+++ src/module-info.java
@@ -1,15 +1,9 @@
 module watamebot.max0rcustom {
 	exports net.foxgenesis.max0r;
 
-	requires java.desktop;
-	requires transitive net.dv8tion.jda;
-	requires transitive org.slf4j;
 	requires transitive watamebot;
-	requires org.apache.commons.configuration2;
-	requires org.apache.commons.text;
 	requires org.json;
 	
-	//provides net.foxgenesis.watame.plugin.IPlugin with net.foxgenesis.max0r.Max0rCustomPlugin;
 	provides net.foxgenesis.watame.plugin.Plugin with net.foxgenesis.max0r.Max0rCustomPlugin;
 
 }
