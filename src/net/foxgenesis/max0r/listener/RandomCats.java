acb7bed81b46076276a8fd717e14dd2cb4b8aff8
Ashley <ashley@foxgenesis.net>
2023-08-01 19:10:15 -0500

    Update RandomCats.java

--- src/net/foxgenesis/max0r/listener/RandomCats.java
+++ src/net/foxgenesis/max0r/listener/RandomCats.java
@@ -3,9 +3,12 @@ package net.foxgenesis.max0r.listener;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import net.foxgenesis.cats.Size;
 import net.foxgenesis.cats.TheCatAPI;
+import net.foxgenesis.cats.bean.Breed;
 import net.foxgenesis.cats.bean.CatPicture;
 import net.foxgenesis.watame.util.Colors;
 import net.foxgenesis.watame.util.Response;
@@ -14,6 +17,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -23,6 +28,10 @@ import okhttp3.OkHttpClient;
 
 public class RandomCats extends ListenerAdapter {
 	private static final Logger logger = LoggerFactory.getLogger(RandomCats.class);
+
+	private static final Emoji EMOJI_HAPPY = Emoji.fromCustom("happeh", 478378484025131010L, false);
+	private static final String FIELD_FORMAT = "**%s:** %s\n";
+	private static final String FLAG_FORMAT = ":flag_%s:";
 
 	private final TheCatAPI api;
 	private final OkHttpClient client;
@@ -38,26 +47,26 @@ public class RandomCats extends ListenerAdapter {
 	public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
 		switch (event.getFullCommandName()) {
 			case "cat" -> {
-				String breed = event.getOption("breed", OptionMapping::getAsString);
-				String[] breeds = breed != null ? new String[] { breed } : null;
+				String breedOption = event.getOption("breed", OptionMapping::getAsString);
+				String[] breeds = breedOption != null ? new String[] { breedOption } : null;
+
+				event.deferReply().queue();
 
 				api.search(client, Size.SMALL, null, null, 0, 1, null, breeds, false, true, true)
-						.whenComplete((list, e) -> {
+						.whenCompleteAsync((list, e) -> {
 							if (e != null) {
 								event.replyEmbeds(Response.error("An error occured. Please try again later.")).queue();
 								logger.error("Error occured during api request", e);
 							} else {
-								CatPicture first = list[0];
-
-								// Construct embed
-								EmbedBuilder builder = new EmbedBuilder();
-								builder.setColor(Colors.INFO);
-								builder.setTitle("Meow!");
-								builder.setImage(first.getUrl());
-								builder.setFooter("via thecatapi.com");
-
-								// Send response
-								event.replyEmbeds(builder.build()).queue();
+								try {
+									// Send response
+									event.getHook().editOriginalEmbeds(createCatEmbed(list[0]))
+											.flatMap(message -> message.addReaction(EMOJI_HAPPY)).queue();
+								} catch (Exception e2) {
+									event.replyEmbeds(Response.error("An error occured. Please try again later."))
+											.queue();
+									logger.error("Error occured during api request", e2);
+								}
 							}
 						});
 			}
@@ -70,23 +79,75 @@ public class RandomCats extends ListenerAdapter {
 			case "cat" -> {
 				switch (event.getFocusedOption().getName()) {
 					case "breed" -> {
-						api.getBreedList(client).whenComplete((breeds, e) -> {
+						// Get all cat breeds
+						api.getBreedList(client).whenCompleteAsync((breeds, e) -> {
 							if (e == null) {
-								String option = event.getFocusedOption().getValue().toLowerCase();
+								// Construct a stream of breeds with all breed names and their alternative names
+								Stream<Pair<String, Breed>> stream = Arrays.stream(breeds)
+										.mapMulti((Breed breed, Consumer<Pair<String, Breed>> consumer) -> {
+											// Add breed name
+											consumer.accept(new Pair<String, Breed>(breed.getName(), breed));
 
-								List<Command.Choice> options = Arrays.stream(breeds)
-										.filter(breed -> breed.getName().toLowerCase().startsWith(option))
-										.map(breed -> new Command.Choice(breed.getName(), breed.getId())).limit(25)
-										.toList();
+											// Add all alternative names if present
+											boolean hasAltNames = !(breed.getAlt_names() == null
+													|| breed.getAlt_names().trim().isBlank());
+											if (hasAltNames)
+												for (String name : breed.getAlt_names().split("[,/]"))
+													consumer.accept(new Pair<String, Breed>(name.trim(), breed));
+										}).sorted((a, b) -> a.key.compareTo(b.key));
 
-								event.replyChoices(options).queue();
-							} else {
-								logger.error("Error occured during api request", e);
+								// Filter stream if user has typed something
+								String option = event.getFocusedOption().getValue();
+								if (!(option == null || option.isBlank())) {
+									String o = option.toLowerCase();
+									stream = stream.filter(pair -> pair.key.toLowerCase().contains(o));
+								}
+
+								// Map results to choices and reply
+								List<Command.Choice> choices = stream
+										.map(pair -> new Command.Choice(pair.key, pair.value.getId())).toList();
+								event.replyChoices(choices.subList(0, Math.min(25, choices.size()))).queue();
 							}
+						}).whenCompleteAsync((v, e) -> {
+							if (e != null)
+								logger.error("Error occured during api request", e);
 						});
 					}
 				}
 			}
 		}
 	}
+
+	private static MessageEmbed createCatEmbed(CatPicture cat) {
+		// Construct embed
+		EmbedBuilder builder = new EmbedBuilder();
+		builder.setColor(Colors.INFO);
+		// builder.setTitle(":cat: Meow!");
+		builder.setImage(cat.getUrl());
+		builder.setFooter("via thecatapi.com", "https://thecatapi.com/favicon.ico");
+
+		// Append breed information if present
+		if (cat.getBreeds() != null && cat.getBreeds().length > 0) {
+			Breed breed = cat.getBreeds()[0];
+
+			StringBuilder b = new StringBuilder();
+			b.append(FIELD_FORMAT.formatted("Breed", breed.getName()));
+			b.append(FIELD_FORMAT.formatted("Origin",
+					FLAG_FORMAT.formatted(breed.getCountry_code().toLowerCase()) + " " + breed.getOrigin()));
+
+			// Add alternative names if present
+			if (!(breed.getAlt_names() == null || breed.getAlt_names().trim().isBlank()))
+				b.append(FIELD_FORMAT.formatted("Alternative Names", breed.getAlt_names()));
+
+			b.append(FIELD_FORMAT.formatted("Temperament", breed.getTemperament()));
+
+			b.append("\n");
+			b.append("[Wikipedia Page](" + breed.getWikipedia_url() + ")");
+
+			builder.setDescription(b.toString().trim());
+		}
+		return builder.build();
+	}
+
+	private static record Pair<T1, T2>(T1 key, T2 value) {}
 }
