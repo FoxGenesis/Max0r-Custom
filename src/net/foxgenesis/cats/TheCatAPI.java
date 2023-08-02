ce74b86042b9aaaeda85dc580c8e30edaf9db955
Ashley <ashley@foxgenesis.net>
2023-08-01 19:10:13 -0500

    Update TheCatAPI.java

--- src/net/foxgenesis/cats/TheCatAPI.java
+++ src/net/foxgenesis/cats/TheCatAPI.java
@@ -3,11 +3,13 @@ package net.foxgenesis.cats;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.function.Supplier;
 
 import net.foxgenesis.cats.bean.Breed;
@@ -36,8 +38,8 @@ public class TheCatAPI {
 
 	private static final String API_URL = "https://api.thecatapi.com/v1";
 
+	private final CopyOnWriteArraySet<Breed> breeds = new CopyOnWriteArraySet<>();
 	private final Optional<String> apiKey;
-	private Breed[] breeds;
 
 	public TheCatAPI(@Nullable String key) {
 		this.apiKey = Optional.ofNullable(key);
@@ -54,32 +56,6 @@ public class TheCatAPI {
 	}
 
 	@NotNull
-	public CompletableFuture<CatPicture[]> search(@NotNull OkHttpClient client, @NotNull Optional<Size> size,
-			@NotNull Optional<MimeType[]> mime_types, @NotNull Optional<Order> order, @NotNull Optional<Integer> page,
-			@NotNull Optional<Integer> limit, @NotNull Optional<int[]> categorys, @NotNull Optional<String[]> breeds,
-			@NotNull Optional<Boolean> hasBreeds, @NotNull Optional<Boolean> includeBreeds,
-			@NotNull Optional<Boolean> includeCategorys) {
-		Objects.requireNonNull(client);
-
-		// Construct query parameters
-		Map<String, String> map = new HashMap<>();
-		map.put("size", size.map(Object::toString).map(String::toLowerCase).orElse(""));
-		map.put("mime_types", mime_types.map(ArrayUtils::commaSeparated).map(String::toLowerCase).orElse(""));
-		map.put("order", order.map(Object::toString).orElse(""));
-		map.put("page", page.map(p -> p + "").orElse(""));
-		map.put("limit", limit.map(l -> l + "").orElse(""));
-		map.put("category_ids", categorys.map(ArrayUtils::commaSeparated).orElse(""));
-		map.put("breed_ids", breeds.map(ArrayUtils::commaSeparated).orElse(""));
-		map.put("onlyBreeds", hasBreeds.map(b -> "" + b.compareTo(false)).orElse(""));
-		map.put("include_breeds", includeBreeds.map(b -> "" + b.compareTo(false)).orElse(""));
-		map.put("include_categories", includeBreeds.map(b -> "" + b.compareTo(false)).orElse(""));
-
-		// Enqueue and map to result
-		return submit(client, newRequest(Method.GET, "images/search", map, null))
-				.thenApply(response -> readResponse(response, CatPicture[].class));
-	}
-
-	@NotNull
 	public CompletableFuture<CatPicture> getPictureFromID(@NotNull OkHttpClient client, @NotNull String id,
 			String subid, Size size, boolean includeVote, boolean includeFavorite) {
 		return getPictureFromID(client, id, Optional.ofNullable(subid), Optional.ofNullable(size),
@@ -87,11 +63,33 @@ public class TheCatAPI {
 	}
 
 	@NotNull
+	public CompletableFuture<CatPicture[]> search(@NotNull OkHttpClient client, @NotNull Optional<Size> size,
+			@NotNull Optional<MimeType[]> mime_types, @NotNull Optional<Order> order, @NotNull Optional<Integer> page,
+			@NotNull Optional<Integer> limit, @NotNull Optional<int[]> categorys, @NotNull Optional<String[]> breeds,
+			@NotNull Optional<Boolean> hasBreeds, @NotNull Optional<Boolean> includeBreeds,
+			@NotNull Optional<Boolean> includeCategorys) {
+		// Construct query parameters
+		Map<String, String> map = new HashMap<>();
+		map.put("size", size.map(Object::toString).map(String::toLowerCase).orElse(""));
+		map.put("mime_types", mime_types.map(ArrayUtils::commaSeparated).map(String::toLowerCase).orElse(""));
+		map.put("order", order.map(Object::toString).orElse(""));
+		map.put("page", page.map(p -> p + "").orElse(""));
+		map.put("limit", limit.map(l -> l + "").orElse(""));
+		map.put("category_ids", categorys.map(c -> joinInts(",", c)).orElse(""));
+		map.put("breed_ids", breeds.map(ArrayUtils::commaSeparated).orElse(""));
+		map.put("onlyBreeds", hasBreeds.map(b -> "" + b.compareTo(false)).orElse(""));
+		map.put("include_breeds", includeBreeds.map(b -> "" + b.compareTo(false)).orElse(""));
+		map.put("include_categories", includeBreeds.map(b -> "" + b.compareTo(false)).orElse(""));
+
+		// Enqueue and map to result
+		return submit(client, newRequest(Method.GET, "images/search", map, null))
+				.thenApplyAsync(response -> readJSONResponse(response, CatPicture[].class));
+	}
+
+	@NotNull
 	public CompletableFuture<CatPicture> getPictureFromID(@NotNull OkHttpClient client, @NotNull String id,
 			@NotNull Optional<String> subid, @NotNull Optional<Size> size, @NotNull Optional<Boolean> includeVote,
 			@NotNull Optional<Boolean> includeFavorite) {
-		Objects.requireNonNull(client);
-
 		// Construct query parameters
 		Map<String, String> map = new HashMap<>();
 		map.put("size", size.map(Object::toString).map(String::toLowerCase).orElse(""));
@@ -101,23 +99,28 @@ public class TheCatAPI {
 
 		// Enqueue and map to result
 		return submit(client, newRequest(Method.GET, "images/" + id, map, null))
-				.thenApply(response -> readResponse(response, CatPicture.class));
+				.thenApplyAsync(response -> readJSONResponse(response, CatPicture.class));
 	}
 
 	@NotNull
 	public CompletableFuture<Breed[]> getBreedList(@NotNull OkHttpClient client) {
-		Objects.requireNonNull(client);
-
-		if (breeds != null)
-			return CompletableFuture.completedFuture(Arrays.copyOf(breeds, breeds.length));
+		if (!breeds.isEmpty())
+			return CompletableFuture.completedFuture(breeds.toArray(Breed[]::new));
 
 		// Enqueue and map to result
 		logger.debug("Getting breed list");
 		return submit(client, newRequest(Method.GET, "breeds", null, null))
-				.thenApply(response -> readResponse(response, Breed[].class)).handle((breeds, err) -> {
+				.thenApplyAsync(response -> readJSONResponse(response, Breed[].class)).handleAsync((breeds, err) -> {
 					if (err == null) {
-						this.breeds = breeds;
-						return Arrays.copyOf(breeds, breeds.length);
+						// Sort breeds by name
+						Arrays.sort(breeds, (a, b) -> a.getName().compareTo(b.getName()));
+
+						// Update breed list
+						synchronized (this.breeds) {
+							this.breeds.clear();
+							this.breeds.addAll(List.of(breeds));
+							return this.breeds.toArray(Breed[]::new);
+						}
 					}
 					return null;
 				});
@@ -126,25 +129,47 @@ public class TheCatAPI {
 	private Request newRequest(Method method, String endpoint, Map<String, String> params, Supplier<RequestBody> body) {
 		Objects.requireNonNull(method);
 
+		// Create URL builder for the specified end point
 		HttpUrl.Builder httpBuilder = HttpUrl.parse(API_URL + '/' + endpoint).newBuilder();
+
+		// Add URL parameters
 		if (params != null)
 			for (Map.Entry<String, String> param : params.entrySet())
 				httpBuilder.addQueryParameter(param.getKey(), param.getValue());
 
+		// Construct a new request builder
 		Builder builder = new Request.Builder();
 		builder.url(httpBuilder.build());
 		builder.addHeader("Content-Type", "application/json");
+
+		// Add API key if present
 		apiKey.ifPresent(key -> builder.addHeader("x-api-key", key));
+
+		// Add request body if specified
 		if (body != null)
 			builder.method(method.name().toUpperCase(), body.get());
 
+		// Build the request
 		return builder.build();
 	}
 
+	/**
+	 * Enqueue a {@link Request} to an {@link OkHttpClient} and map it's
+	 * {@link Callback} to a {@link CompletableFuture}.
+	 * 
+	 * @param client  - HTTP client to use
+	 * @param request - request to enqueue
+	 * 
+	 * @return Returns a {@link CompletableFuture} that will complete normally when
+	 *         the {@link Callback#onResponse(Call, Response)} is called. Otherwise
+	 *         will complete exceptionally if the
+	 *         {@link Callback#onFailure(Call, IOException)} is called.
+	 */
 	private static CompletableFuture<Response> submit(OkHttpClient client, Request request) {
-		logger.debug("{} {}", request.method().toUpperCase(), request.url().toString());
-
+		Objects.requireNonNull(client);
 		CompletableFuture<Response> callback = new CompletableFuture<>();
+
+		logger.debug("{} {}", request.method().toUpperCase(), request.url().toString());
 		client.newCall(request).enqueue(new Callback() {
 			@Override
 			public void onFailure(Call arg0, IOException arg1) {
@@ -159,12 +184,41 @@ public class TheCatAPI {
 		return callback;
 	}
 
-	private static <T> T readResponse(Response response, Class<? extends T> c) {
+	/**
+	 * Map a {@link Response} to a JavaBean. The specified response's content type
+	 * must be {@code application/json}.
+	 * 
+	 * @param <T>      JavaBean class
+	 * @param response - response to get data from
+	 * @param javaBean - JavaBean class to map JSON to
+	 * 
+	 * @return Returns an instance of the {@code javaBean} that was constructed with
+	 *         the response's {@link okhttp3.ResponseBody#string() body} content
+	 */
+	private static <T> T readJSONResponse(Response response, Class<? extends T> javaBean) {
+		// Ensure content type is application/json
+		if (!response.body().contentType().subtype().equals("json"))
+			throw new CompletionException(new IOException("Returned content type is not application/json"));
+
+		// Construct the JavaBean from the response body
 		ObjectMapper mapper = new ObjectMapper();
 		try (JsonParser parser = mapper.createParser(response.body().string())) {
-			return mapper.readValue(parser, c);
+			return mapper.readValue(parser, javaBean);
 		} catch (IOException e) {
 			throw new CompletionException(e);
 		}
+	}
+
+	private static String joinInts(String delim, int[] arr) {
+		if (arr == null)
+			return null;
+
+		String out = "";
+		for (int i = 0; i < arr.length; i++) {
+			if (i != 0)
+				out += ',';
+			out += arr[i];
+		}
+		return out;
 	}
 }
