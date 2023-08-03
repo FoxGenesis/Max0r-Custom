615977aa9e39d0c5994dba3ecd2707e30dd8e7b0
Ashley <ashley@foxgenesis.net>
2023-08-02 19:12:17 -0500

    added optional params

--- src/net/foxgenesis/cats/bean/CatPicture.java
+++ src/net/foxgenesis/cats/bean/CatPicture.java
@@ -3,11 +3,23 @@ package net.foxgenesis.cats.bean;
 import java.util.Arrays;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CatPicture {
 	private String id;
 	private String url;
 	private int width;
 	private int height;
+
+	@JsonProperty(required = false)
+	private String sub_id;
+	@JsonProperty(required = false)
+	private String created_at;
+	@JsonProperty(required = false)
+	private String original_filename;
+	@JsonProperty(required = false)
+	private String[] breed_ids;
+
 	private Breed[] breeds;
 	private Category[] categories;
 
@@ -41,6 +53,38 @@ public class CatPicture {
 
 	public void setHeight(int height) {
 		this.height = height;
+	}
+
+	public String getSub_id() {
+		return sub_id;
+	}
+
+	public void setSub_id(String sub_id) {
+		this.sub_id = sub_id;
+	}
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+
+	public String getOriginal_filename() {
+		return original_filename;
+	}
+
+	public void setOriginal_filename(String original_filename) {
+		this.original_filename = original_filename;
+	}
+
+	public String[] getBreed_ids() {
+		return breed_ids;
+	}
+
+	public void setBreed_ids(String[] breed_ids) {
+		this.breed_ids = breed_ids;
 	}
 
 	public Breed[] getBreeds() {
