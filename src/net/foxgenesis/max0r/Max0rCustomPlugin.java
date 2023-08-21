fb42291c0dc792502b0bead475ae5ec735665af9
Ashley <ashley@foxgenesis.net>
2023-08-20 21:04:22 -0500

    1.1.2

--- src/net/foxgenesis/max0r/Max0rCustomPlugin.java
+++ src/net/foxgenesis/max0r/Max0rCustomPlugin.java
@@ -1,30 +1,17 @@
 package net.foxgenesis.max0r;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
-
 import net.foxgenesis.max0r.listener.DadListener;
 import net.foxgenesis.max0r.listener.EmbedPermsListener;
 import net.foxgenesis.max0r.listener.NonPingableNameListener;
 import net.foxgenesis.max0r.listener.VoiceChatListener;
 import net.foxgenesis.watame.WatameBot;
-import net.foxgenesis.watame.plugin.CommandProvider;
 import net.foxgenesis.watame.plugin.IEventStore;
 import net.foxgenesis.watame.plugin.Plugin;
 import net.foxgenesis.watame.plugin.SeverePluginException;
 import net.foxgenesis.watame.property.PluginPropertyProvider;
 
-import org.apache.commons.configuration2.Configuration;
-
-import net.dv8tion.jda.api.interactions.commands.build.CommandData;
-
-public class Max0rCustomPlugin extends Plugin implements CommandProvider {
+public class Max0rCustomPlugin extends Plugin {
 	private NonPingableNameListener pingable;
-
-	@Override
-	protected void onConstruct(Properties meta, Map<String, Configuration> configs) {}
 
 	@Override
 	protected void preInit() throws SeverePluginException {}
@@ -51,9 +38,4 @@ public class Max0rCustomPlugin extends Plugin implements CommandProvider {
 
 	@Override
 	protected void close() {}
-
-	@Override
-	public Collection<CommandData> getCommands() {
-		return Set.of();
-	}
 }
